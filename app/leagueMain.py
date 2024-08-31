31f10b38828d5266a1a0227e40775efdfcbc0f9e
Kyle-Kiet Chau <kylekietchau@gmail.com>
2024-08-30 21:55:44 -0400

    Fixed error from render

--- app/leagueMain.py
+++ app/leagueMain.py
@@ -29,7 +29,6 @@ def submit_data():
     tagLine = data["tagline"]
     region = data["region"].lower()
     numMatches = data["num"]
-    headers = {}
 
     api_url = f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}?api_key={api_key}"
 
@@ -40,17 +39,13 @@ def submit_data():
 
         # Fetches a list of match id's for a player given number of matches and player id
         match_id_url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={numMatches}&api_key={api_key}"
-        match_id_response = requests.get(match_id_url, headers=headers)
+        match_id_response = requests.get(match_id_url)
         match_id_data = match_id_response.json()
 
         games = []
         totalK = 0
         totalD = 0
         totalDMG = 0
-
-        dict = {
-            'X-Riot-Token': api_key
-        }
 
         with ThreadPoolExecutor(max_workers=10) as executor:
             count =  0
@@ -60,6 +55,7 @@ def submit_data():
                 if (count % 15 == 0):
                     time.sleep(1.01)
                 count += 1
+                
                 future = executor.submit(fetch_match_data, match_id, api_key, region)
                 dict[future] = match_id
 
@@ -70,7 +66,7 @@ def submit_data():
                 try:    #In case the user runs out of api calls midway through calling each match
                     inGameOrder = individual_match_data["metadata"]["participants"].index(f"{puuid}")
                 except Exception as e:
-                    return jsonify({"status": "error", "header": individual_match_data.headers, "message": individual_match_data["status"]["message"]}), individual_match_data["status"]["status_code"]
+                    return jsonify({"status": "error", "message": individual_match_data["status"]["message"]}), individual_match_data["status"]["status_code"]
                 
                 player = individual_match_data["info"]["participants"][inGameOrder]
 
